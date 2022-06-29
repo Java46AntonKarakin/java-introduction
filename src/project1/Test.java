c656fc323fa66f961b42cf67b01d08301b54f9ce
volnykot <volnykot3@gmail.com>
2022-06-29 18:01:03 +0300

    No one allowed to distort Cthulhu's name :(

--- src/project1/Test.java
+++ src/project1/Test.java
@@ -1,7 +1,7 @@
 package project1;
 
 public class Test {
-public static void main(String[] args) {
-	System.out.println ("Ktulhu Fhtang !!");
-}
+	public static void main(String[] args) {
+		System.out.println("Cthulhu Fhtagn !!!");
+	}
 }
