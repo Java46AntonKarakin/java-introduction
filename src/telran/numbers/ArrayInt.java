f180a6f712f33cacba9c3f385f55c372844d0f2d
volnykot <volnykot3@gmail.com>
2022-07-04 18:03:55 +0300

    Java-46 HW-03 with SportLoto and completed tests. Some bugs are fixed.

--- src/telran/numbers/ArrayInt.java
+++ src/telran/numbers/ArrayInt.java
@@ -5,9 +5,9 @@ import java.util.Arrays;
 public class ArrayInt {
 
 	public static int[] addNumber(int ar[], int number) {
-		int[] localArr = Arrays.copyOf(ar, ar.length + 1);
-		localArr[localArr.length - 1] = number;
-		return localArr;
+		int[] res = Arrays.copyOf(ar, ar.length + 1);
+		res[res.length - 1] = number;
+		return res;
 	}
 
 	/**
@@ -19,10 +19,10 @@ public class ArrayInt {
 	 */
 
 	public static int[] insertNumber(int ar[], int index, int number) {
-		int[] localArr = Arrays.copyOf(ar, ar.length + 1);
-		System.arraycopy(ar, index, localArr, index + 1, ar.length - index);
-		localArr[index] = number;
-		return localArr;
+		int[] res = Arrays.copyOf(ar, ar.length + 1);
+		System.arraycopy(ar, index, res, index + 1, ar.length - index);
+		res[index] = number;
+		return res;
 		// solution should be based on the method arraycopy of the class System
 		// (System.arraycopy)
 	}
@@ -36,15 +36,15 @@ public class ArrayInt {
 	public static int[] removeNumber(int ar[], int index) {
 
 		int[] localArr = new int[ar.length - 1];
-		
+
 		if (index == 0) {
 			System.arraycopy(ar, 1, localArr, 0, ar.length - 1);
 		}
 
 		if (index != 0 && index > 0 && index != ar.length - 1) {
-			System.arraycopy(ar, 0, localArr, 0, index);			
+			System.arraycopy(ar, 0, localArr, 0, index);
 			System.out.println("localArr = " + Arrays.toString(localArr));
-			System.arraycopy(ar, index+1, localArr, index, ar.length - 2);
+			System.arraycopy(ar, index + 1, localArr, index, ar.length - 2);
 		}
 
 		if (index == ar.length - 1) {
--- src/telran/numbers/SportLotoAppl.java
+++ src/telran/numbers/SportLotoAppl.java
@@ -9,37 +9,38 @@ import java.util.Arrays;
  */
 
 public class SportLotoAppl {
-	public static final int maxValue = 49;
 	public static final int minValue = 1;
+	public static final int maxValue = 49;
 
 	public static void main(String[] args) {
 		System.out.println(Arrays.toString(SportLoto()));
 	}
 
 	private static int[] SportLoto() {
-													// 	creates rundom array
+
+		// creates random array
+
 		int[] res = new int[7];
-		for (int i = 0; i < res.length;) { 									
-			int tempRandomNumber = getRandomNumber(minValue, maxValue);
-			if (res[i] != tempRandomNumber) {
-				res[i] = tempRandomNumber;
-				i++;
-			}
-		}		
-													// checking random array if there is any repeated meanings		
-		
-		for (int j = 0; j < res.length - 1; j++) {			
-			for (int i = j + 1; i < res.length;i++) {
+		for (int i = 0; i < res.length; i++) {
+			res[i] = getRandomNumber(minValue, maxValue);
+		}
+
+		// checking random array if there are any repeated meanings
+
+		for (int i = 0; i < res.length; i++) {
+			for (int j = i + 1; j < res.length; j++) {
 				if (res[i] == res[j]) {
-					res[i] = getRandomNumber(minValue, maxValue);
+					res[j] = getRandomNumber(minValue, maxValue);
+					i = 0;
+					j = 1;
 				}
 			}
 		}
-
 		return res;
 	}
-	
-													// helper method for getting one random number in the range [min-max]
+
+	// helper method for getting one random number in the range [min-max]
+
 	private static int getRandomNumber(int min, int max) {
 		return (int) (min + Math.random() * (max - min + 1));
 	}
