c17e0b70e550e4e7137af4e820bcdf0770121c11
volnykot <volnykot3@gmail.com>
2022-07-06 13:05:22 +0300

    add HW#4 binarySearch done method

--- src/telran/numbers/ArrayInt.java
+++ src/telran/numbers/ArrayInt.java
@@ -3,11 +3,17 @@ package telran.numbers;
 import java.util.Arrays;
 
 public class ArrayInt {
-
+	/**
+	 * 
+	 * @param ar
+	 * @param number
+	 * @return new array with added number
+	 */
 	public static int[] addNumber(int ar[], int number) {
-		int[] res = Arrays.copyOf(ar, ar.length + 1);
-		res[res.length - 1] = number;
+		int res[] = Arrays.copyOf(ar, ar.length + 1);
+		res[ar.length] = number;
 		return res;
+
 	}
 
 	/**
@@ -17,14 +23,12 @@ public class ArrayInt {
 	 * @param number
 	 * @return new array containing number value at the given index
 	 */
-
 	public static int[] insertNumber(int ar[], int index, int number) {
-		int[] res = Arrays.copyOf(ar, ar.length + 1);
-		System.arraycopy(ar, index, res, index + 1, ar.length - index);
+		int res[] = new int[ar.length + 1];
+		System.arraycopy(ar, 0, res, 0, index);
 		res[index] = number;
+		System.arraycopy(ar, index, res, index + 1, ar.length - index);
 		return res;
-		// solution should be based on the method arraycopy of the class System
-		// (System.arraycopy)
 	}
 
 	/**
@@ -34,32 +38,11 @@ public class ArrayInt {
 	 * @return new array with removed number at the given index
 	 */
 	public static int[] removeNumber(int ar[], int index) {
+		int res[] = new int[ar.length - 1];
+		System.arraycopy(ar, 0, res, 0, index);
+		System.arraycopy(ar, index + 1, res, index, res.length - index);
+		return res;
 
-		int[] localArr = new int[ar.length - 1];
-
-		if (index == 0) {
-			System.arraycopy(ar, 1, localArr, 0, ar.length - 1);
-		}
-
-		if (index != 0 && index > 0 && index != ar.length - 1) {
-			System.arraycopy(ar, 0, localArr, 0, index);
-			System.out.println("localArr = " + Arrays.toString(localArr));
-			System.arraycopy(ar, index + 1, localArr, index, ar.length - 2);
-		}
-
-		if (index == ar.length - 1) {
-			System.arraycopy(ar, 0, localArr, 0, ar.length - 1);
-		}
-
-		if (index == 0) {
-			System.arraycopy(ar, 1, localArr, 0, ar.length - 1);
-		}
-		System.out.println();
-
-		return localArr;
-
-		// solution should be based on the method arraycopy of the class System
-		// (System.arraycopy)
 	}
 
 	/**
@@ -77,6 +60,77 @@ public class ArrayInt {
 				break;
 			}
 		}
+		return res;
+	}
+
+	public static void sort(int[] ar) {
+		for (int i = 0; i < ar.length; i++) {
+			moveMaxToEnd(ar);
+		}
+		// TODO
+		// Improve algorithm with a proper moveMaxToEnd method call
+
+	}
+
+	private static void moveMaxToEnd(int[] ar) {
+		for (int i = 1; i < ar.length; i++) {
+			if (ar[i - 1] > ar[i]) {
+				swap(ar, i);
+			}
+		}
+		// TODO
+		// Improve algorithm of moveMaxToEnd:
+		// (1) no compare of elements that already exist on the proper places
+		// (think of additional parameter of the method with code update)
+		// (2) terminate algorithm once an array is already sorted (think of returning
+		// some
+		// value with code update)
+
+	}
+
+	private static void swap(int[] ar, int index) {
+		int tmp = ar[index - 1];
+		ar[index - 1] = ar[index];
+		ar[index] = tmp;
+
+	}
+
+	/**
+	 * 
+	 * @param ar     - sorted array
+	 * @param number
+	 * @return if the given number exists in the given array then returns the index
+	 *         of first occurrence, otherwise returns negative value of (index + 1)
+	 *         where index is the one at which the given number should have been
+	 */
+	public static int binaryIndexOf(int[] ar, int number) {
+		int res = 0;
+		int left = 0;
+		int right = ar.length - 1;
+		int middle = ar.length / 2;
+
+		while (left <= right && ar[middle] != number) {
+			if (ar[middle] < number) {
+				left = middle + 1; // looking for the number will be in right part of the array
+			} else {
+				right = middle - 1; // looking for the number will be in the left part of the array
+			}
+			middle = (left + right) / 2;
+		}
+		int temp = left > right ? -1 : middle;
+		/*----------------------looking for the 1st index of founded number----------------------------------------- */
+
+		if (temp != -1) {
+			res = indexOf(ar, number);
+		} else {
+			res = temp;
+		}
+
+		/*----------------------looking for bogeyman--------------------------------------------------------------- */
+		if (temp == -1) {
+			res = (left + 1) * -1;
+		}
+
 		return res;
 	}
 }
