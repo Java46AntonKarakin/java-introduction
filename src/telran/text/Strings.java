80642e4e373f5a99744ca43d304b121db006ea85
volnykot <volnykot3@gmail.com>
2022-07-13 19:16:01 +0300

    add HW7 checked

--- src/telran/text/Strings.java
+++ src/telran/text/Strings.java
@@ -1,29 +1,8 @@
 package telran.text;
 
+import java.util.Arrays;
+
 public class Strings {
-	static public String join(String[] array, String delimiter) {
-		// STring "+" operator based solution
-		return stringPluseSolution(array, delimiter);
-		// StringBuilder attend based solution
-		// return stringBuilderSolution(array, delimiter);
-	}
-
-	static private String stringBuilderSolution(String[] array, String delimiter) {
-		StringBuilder strBuilder = new StringBuilder(array[0]);
-		for (int i = 1; i < array.length; i++) {
-			strBuilder.append(delimiter).append(array[i]);
-		}
-		return strBuilder.toString();
-	}
-
-	static private String stringPluseSolution(String[] array, String delimiter) {
-		String res = array[0]; // assumption: there is at least one string
-		for (int i = 1; i < array.length; i++) {
-			res += delimiter + array[i];
-		}
-		return res;
-	}
-
 	/**
 	 * 
 	 * @param name1 - first name
@@ -32,60 +11,53 @@ public class Strings {
 	 */
 	public static String matches(String name1, String name2) {
 
-		name1 = name1.trim().toUpperCase();
-		name2 = name2.trim().toUpperCase();
+		String[] arrName1 = name1.trim().split(" ");
+		String[] arrName2 = name2.trim().split(" ");
 
-		if (name1.equals(name2)) {
+		if (name1.equalsIgnoreCase(name2)) {
 			return "match";
 		}
-		String[] arrName1 = name1.split(" ");
-		String[] arrName2 = name2.split(" ");
 
 		if (arrName1.length == arrName2.length) {
-
-			int positionOfAbbr = isAbbreviation(arrName1);
-
-			if (positionOfAbbr >= 0) {
-				if (isAbbrMatch(arrName1, arrName2, positionOfAbbr)) {
-					return "match";
-				}
-				return "no match";
-			}
-		}
-		if (arrName1.length > arrName2.length) {
-			if (isSecondEqualsThird(arrName1, arrName2)) {
+			if (isAbbreviation1(arrName1, arrName2)) {
 				return "match";
 			}
 		}
+
+		if (arrName1.length > arrName2.length) {
+			if (isMiddleNameOmited(arrName1, arrName2)) {
+				return "match";
+			}
+		}
+
 		return "no match";
 	}
 
-	private static boolean isSecondEqualsThird(String[] arrName1, String[] arrName2) {
-		for (int i = 0; i < arrName1.length - 1; i++) {
-			if (arrName1[i].equals(arrName2[0])) {
+	private static boolean isMiddleNameOmited(String[] arrName1, String[] arrName2) {
+		for (int i = 1; i <= arrName2.length; i++) {
+			if (arrName1[i].equalsIgnoreCase(arrName2[i - 1])) {
 				return true;
 			}
 		}
 		return false;
 	}
 
-	private static boolean isAbbrMatch(String[] arrName1, String[] arrName2, int positionOfAbbr) {
-		for (int i = 0; i < arrName2.length; i++) {
-			if (arrName2[i].startsWith(arrName1[positionOfAbbr])) {
-				return true;
-			}
-		}
-		return false;
-	}
-
-	private static int isAbbreviation(String[] arrName1) {
-		int res = -1;
+	private static boolean isAbbreviation1(String[] arrName1, String[] arrName2) {
+		int abr = 0;
 		for (int i = 0; i < arrName1.length; i++) {
 			if (arrName1[i].length() == 1) {
-				res = i;
+				abr = i;
+				break;
 			}
 		}
-		return res;
+		if (abr > 0) {
+			for (int j = 0; j < arrName2.length; j++) {
+				if (arrName2[j].startsWith(arrName1[abr])) {
+					return true;
+				}
+			}
+		}
+		return false;
 	}
 
 	/**
@@ -96,99 +68,51 @@ public class Strings {
 	 *                   String containing "123" should be greater than string
 	 *                   containing "23" as the number 123 greater than number 23
 	 */
-	static public String[] sortStringsAsNumbers(String[] strNumbers) {
-		int[] countArr = new int[1000];
+
+	static public void sortStringsAsNumbers(String[] strNumbers) {
+		int[] lut = new int[1000];
 		int temp = 0;
 
 		for (int i = 0; i < strNumbers.length; i++) {
-			countArr[Integer.valueOf(strNumbers[i])]++;
+			lut[Integer.valueOf(strNumbers[i])]++;
 		}
 
-		for (int i = 0; i < countArr.length; i++) {
-			if (countArr[i] > 0) {
-				for (int j = 0; j < countArr[i]; j++) {
+		for (int i = 0; i < lut.length; i++) {
+			if (lut[i] > 0) {
+				for (int j = 0; j < lut[i]; j++) {
 					strNumbers[temp] = String.valueOf(i);
 					temp++;
 				}
 			}
 		}
-		return strNumbers;
+		
 	}
-	
-	public static String[] randomArr(int max) {
-		String[] ar = new String[max];
+
+	// methods for creating array [0...999] for additional test---------------
+
+	public static int[] randomIntArr(int max) {
+		int[] ar = new int[max];
 		for (int i = 0; i < ar.length; i++) {
-			ar[i] = String.valueOf((int) (1 + Math.random() * (max - 0 + 1)));
+			ar[i] = (int) (1 + Math.random() * (max - 0 + 1));
 		}
 		return ar;
 	}
 
-	/*----------old stuff below---------- */
-	public static int[] deepNoRepeatedCompare(String str1, String str2) {
-		int[] res = { 0, 0 };
-		int smallestArrayLength = (str1.length() <= str2.length()) ? str1.length() : str2.length();
-
-		for (int i = 0; i < smallestArrayLength; i++) {
-			if (str1.charAt(i) == str2.charAt(i)) {
-				res[0]++;
-				continue;
-			}
-			if (str1.indexOf(str2.charAt(i)) > -1) {
-				res[1]++;
-			}
+	public static String[] intArrToSortedStrArr(int[] unsortedIntArr) {
+		String[] sortedStrArr = new String[unsortedIntArr.length];
+		Arrays.sort(unsortedIntArr);
+		for (int i = 0; i < unsortedIntArr.length; i++) {
+			sortedStrArr[i] = String.valueOf(unsortedIntArr[i]);
 		}
-		return res;
+		return sortedStrArr;
 	}
 
-	public static int[] deepNoRepeatedCompareOld(String str1, String str2) {
+	public static String[] intArrToStrArr(int[] unsortedIntArr) {
 
-		int[] res = { 0, 0 };
-		int str1Length = str1.length();
-		int str2Length = str2.length();
-
-		for (int i = 0; i < str1Length; i++) {
-			for (int j = 0; j < str2Length; j++) {
-				if (str1.charAt(i) == str2.charAt(j)) {
-					if (i == j) {
-						res[0]++;
-					} else {
-						res[1]++;
-					}
-				}
-			}
+		String[] unSortedStrArr = new String[unsortedIntArr.length];
+		for (int i = 0; i < unSortedStrArr.length; i++) {
+			unSortedStrArr[i] = String.valueOf(unsortedIntArr[i]);
 		}
-		return res;
-	}
-
-	public static boolean isAnagram(String str1, String str2) {
-
-		return checkQuantity(str1, str2) ? (checkQuality(str1, str2) ? true : false) : false;
-	}
-
-	private static boolean checkQuantity(String str1, String str2) {
-		if (str1.length() != str2.length()) {
-			return false;
-		}
-		return true;
-	}
-
-	private static boolean checkQuality(String str1, String str2) {
-		int[] matchResultArr = new int[26];
-		str1 = str1.toLowerCase();
-		str2 = str2.toLowerCase();
-		for (int i = 0; i < str1.length(); i++) {
-			matchResultArr[str1.charAt(i) - 'a']++;
-			matchResultArr[str2.charAt(i) - 'a']--;
-		}
-		return (checkEquality(matchResultArr)) ? true : false;
-	}
-
-	private static boolean checkEquality(int[] matchingArr) {
-		for (int i = 0; i < matchingArr.length; i++) {
-			if (matchingArr[i] != 0) {
-				return false;
-			}
-		}
-		return true;
+		return unSortedStrArr;
 	}
 }
--- src/telran/text/tests/StringsClassTests.java
+++ src/telran/text/tests/StringsClassTests.java
@@ -10,77 +10,9 @@ import telran.text.Strings;
 
 class StringsClassTests {
 
-	private static final int MAX = 10000;
-	private static final int N_RUNS = 10000;
-	private static final int N_STRINGS = 10000;
-
-	
-
-	
-	
-	
-	
-	
-	
-	
-	@Test
-	void deepCompareTest() {
-		String str1 = "123,tT89";
-		String str2 = "123,tT89";
-		String str3 = "19";
-		String str5 = "98Tt,321";
-		String str4 = "kugy";
-		int expected1[] = { 8, 0 };
-		int expected2[] = { 1, 1 };
-		int expected3[] = { 0, 8 };
-		int expected4[] = { 0, 0 };
-		// for (int i = 0; i < N_RUNS; i++) {
-		assertArrayEquals(expected1, Strings.deepNoRepeatedCompare(str1, str2));
-		assertArrayEquals(expected2, Strings.deepNoRepeatedCompare(str1, str3));
-		assertArrayEquals(expected3, Strings.deepNoRepeatedCompare(str1, str5));
-		assertArrayEquals(expected4, Strings.deepNoRepeatedCompare(str1, str4));
-		// }
-	}
-
-	@Test
-	void isAnagramTests() {
-		String str1 = "Hello";
-		String str2 = "Helll";
-		String str3 = "eloHl";
-		String str4 = "Helo";
-		String str5 = "lllll";
-		assertTrue(Strings.isAnagram(str1, str3));
-		assertFalse(Strings.isAnagram(str1, str2));
-		assertFalse(Strings.isAnagram(str1, str4));
-		assertTrue(Strings.isAnagram(str1, str1));
-		assertFalse(Strings.isAnagram(str1, str5));
-	}
-
-	@Test
-	void joinTest() {  
-		String array[] = { "Hello", "Vasya" };
-		String expected = "Hello Vasya";
-		assertEquals(expected, Strings.join(array, " "));
-	}
-
-//	@Test
-//	void joinPerformanceTest() {
-//		String array[] = getBigArray();
-//		for (int i = 0; i < N_RUNS; i++) {
-//			Strings.join(array, " ");
-//		}
-//	}
-
-	private String[] getBigArray() {
-		String res[] = new String[N_STRINGS];
-		for (int i = 0; i < res.length; i++) {
-			res[i] = "Hello";
-		}
-		return res;
-	}
-
 	@Test
 	void matchesTest () {
+		
 //      - David vs. david: match
 //      - John F vs. John Fitzgerald: match
 //      - John K vs. John Fitzgerald: no match
@@ -90,36 +22,43 @@ class StringsClassTests {
 //      - Anna Maria Magdalena vs. Anna Magdalena Roberta: no match
 //      - Anna Maria Magdalena vs. Anna Magdalena Maria: no match
 		
-		String s1 = "John F";
-		String s2 = "John K";
-		String s3 = "John Fitzgerald";
 		
-		String s4 = "Anna Maria Magdalena";
-		String s5 = "Anna Magdalena";
-		String s6 = "Maria Magdalena";
-		String s7 = "Anna Maria Roberta";
-		String s8 = "Anna Magdalena Roberta";
-		String s9 = "Anna Magdalena Maria";
+		String [][] testArray = {
+				{"David", "david", "match"},
+				{"John F", "John Fitzgerald", "match"},
+				{"John K", "John Fitzgerald", "no match"},
+				{"Anna Maria Magdalena", "Anna Magdalena", "match"},
+				{"Anna Maria Magdalena", "Maria Magdalena", "match"},
+				{"Anna Maria Magdalena", "Anna Maria Roberta", "no match"},
+				{"Anna Maria Magdalena", "Anna Magdalena Roberta", "no match"},
+				{"Anna Maria Magdalena", "Anna Magdalena Maria", "no match"}
+				};
 		
-		String match = "match";
-		String noMatch = "no match";
-		
-		assertEquals(match, Strings.matches(s1,s3));  // John F - John Fitzgerald
-		assertEquals(noMatch, Strings.matches(s2,s3)); // John K - John Fitzgerald
-		
-		assertEquals(match, Strings.matches(s4,s5));
-		assertEquals(match, Strings.matches(s4,s6));
-		assertEquals(noMatch, Strings.matches(s4,s7));
-		assertEquals(noMatch, Strings.matches(s4,s8));
-		assertEquals(noMatch, Strings.matches(s4,s9));
-		
+		for (int i =0; i<testArray.length;i++) {
+			assertEquals(testArray[i][2], Strings.matches(testArray[i][0],testArray[i][1]));
+		}
 	}
 
 	@Test
 	void sortStringsAsNumbersTest() {
-		String[] randomArr = {"9", "250", "50", "123", "23", "9", "123", "50"} ;
-		String[] expected = {"9", "9", "23", "50", "50", "123", "123", "250"} ;
-		assertTrue(Arrays.deepEquals(expected,Strings.sortStringsAsNumbers(randomArr)));
+		String[] randomArr = { "999", "9", "250", "50", "123", "23", "9", "123", "50", "0","999", "0"} ;
+		String[] expected = {"0", "0", "9", "9", "23", "50", "50", "123", "123", "250", "999", "999"} ;		
+//		assertTrue(Arrays.deepEquals(expected,Strings.sortStringsAsNumbers(randomArr))); // return type of the 
+//															1st generation of method sortStringsAsNumbers was String
+		
+		Strings.sortStringsAsNumbers(randomArr);
+		assertArrayEquals(expected,randomArr);
+
+//			 additional test
+		
+		int [] unsortedRandomArr = Strings.randomIntArr (998);
+		String[] randomArr2 = Strings.intArrToStrArr(unsortedRandomArr);
+		String[] expected2 = Strings.intArrToSortedStrArr(unsortedRandomArr);
+//		assertTrue(Arrays.deepEquals(expected2,Strings.sortStringsAsNumbers(randomArr2))); // return type of the 
+// 															1st generation of method sortStringsAsNumbers was String
+		
+		Strings.sortStringsAsNumbers(randomArr2);
+		assertArrayEquals(expected2,randomArr2);
 	}
 
 }
